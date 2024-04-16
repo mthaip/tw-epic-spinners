c82a883c842c24325979cc32880bcf922960a314
mthaip <fanmt.fmt@gmail.com>
2024-04-16 23:46:34 +0200

    feat: Build documentation page

--- src/demo/pages/Documentation.vue
+++ src/demo/pages/Documentation.vue
@@ -1,16 +1,79 @@
 <template>
-  <div class="mt-20">
-    <div class="flex flex-col items-center gap-4">
-      <h1 class="text-3xl font-bold text-sky-600">Comming soon!</h1>
+  <div class="mx-auto max-w-4xl p-6 pb-16">
+    <div class="mb-10 flex flex-row items-center justify-between">
       <RouterLink
         to="/"
-        class="flex-1 text-end transition-colors hover:text-sky-600 hover:underline"
-        >Go back</RouterLink
+        class="flex flex-1 flex-row items-center gap-2 transition-colors hover:text-sky-600"
       >
+        <span class="material-symbols-outlined"> keyboard_backspace </span>
+        Home
+      </RouterLink>
+
+      <a
+        class="block"
+        href="https://github.com/mthaip/tw-epic-spinners"
+      >
+        <Github class="h-8 w-8 text-sky-600" />
+      </a>
     </div>
+    <div
+      class="markdown-body"
+      v-html="markdown.render(readme)"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import readme from '../../../README.md?raw';
+import Github from '../components/logos/Github.vue';
+
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+
+const markdown = new MarkdownIt({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value;
+      } catch (__) {}
+    }
+
+    return '';
+  },
+});
 </script>
+
+<style>
+.markdown-body {
+  @apply w-full bg-white text-black;
+
+  & h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    @apply border-b-gray-300;
+  }
+
+  & ul {
+    @apply list-disc;
+  }
+
+  & code {
+    @apply bg-gray-300;
+  }
+
+  & a {
+    @apply text-sky-600;
+  }
+
+  & pre {
+    @apply bg-sky-50;
+
+    & code {
+      @apply text-gray-800;
+    }
+  }
+}
+</style>
