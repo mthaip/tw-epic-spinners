1c7b905f4cc0c50be4b4876c3e73e30b3c25a97a
Thai Phan <minh-thai.phan@towa.at>
2024-04-18 00:54:48 +0200

    feat: Add Playground boilerplate

---
+++ src/demo/pages/Playground.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-container">
+    <Navigation>
+      <template #right>
+        <div class="flex flex-row">
+          <RouterLink
+            to="/docs"
+            class="ml-auto flex items-center px-2 text-end text-sm font-semibold transition-colors hover:text-violet-600 dark:text-white"
+          >
+            Docs
+          </RouterLink>
+        </div>
+      </template>
+    </Navigation>
+  </div>
+
+  <div class="text-white">My playground</div>
+
+  <Footer />
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+import Navigation from '../components/Navigation.vue';
+import Footer from '../components/Footer.vue';
+</script>
