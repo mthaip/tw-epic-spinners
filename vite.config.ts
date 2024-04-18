d04c6f152641d84603a0f1e7c36b74bb83493b0d
mthaip <fanmt.fmt@gmail.com>
2024-04-18 12:20:25 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import mdPlugin from 'vite-plugin-markdown';
-
-// TODO
-// const alias = Object.entries(tsConfig.compilerOptions.paths).reduce(([]));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   assetsInclude: ['**/*.md'],
-  // TODO
-  // resolve: {
-  //   alias: {
-  //     '~': path.resolve(__dirname, './src'),
-  //   },
-  // },
 });
