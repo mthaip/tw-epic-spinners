e6f727abf73f9570c658bacb4ad2e42c769524a8
mthaip <fanmt.fmt@gmail.com>
2025-07-29 12:20:20 +0200

    feat(demo): SEO setup

--- src/demo/layouts/BaseLayout.vue
+++ src/demo/layouts/BaseLayout.vue
@@ -19,9 +19,32 @@
 
 <script setup lang="ts">
 import { Suspense } from 'vue';
+import { useHead } from '@unhead/vue';
 
 import Navigation from '../components/Navigation.vue';
 import Footer from '../components/Footer.vue';
 
 import { creator } from '../../index';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const baseHead = {
+  title: 'Tailwind Epic Spinners',
+  htmlAttrs: { lang: 'en-US' },
+  meta: [{ name: 'robots', content: 'index, follow' }],
+};
+
+if (route.meta.title) {
+  baseHead.title = route.meta.title as string;
+}
+
+if (route.meta.description) {
+  baseHead.meta.push({
+    name: 'description',
+    content: route.meta.description as string,
+  });
+}
+
+useHead(baseHead);
 </script>
